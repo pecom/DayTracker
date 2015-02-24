99c0d721f49c78705ed659ee0f632fad9865b31f
pecom <padari@raleighcharterhs.org>
2015-02-24 12:43:34 -0500

    Anytime
    You can now open the .txt file anytime (aside from the few milliseconds
    it is adding to the file). I'm going to add a search so you can see how
    long you've spent on one thing.

--- whatIDid.py
+++ whatIDid.py
@@ -2,7 +2,6 @@ import time
 import math
 
 #Init of variables
-f = open('Day Tracker.txt', 'a+')   #Creates the Day Tracker.txt if it doesn't exist, or adds on to it
 startTime = []  #List of the start times for my events
 startTimeSeconds = []
 eventList = []  #List of my events
@@ -17,6 +16,7 @@ def addEvent():
     eventList.append(s)
 
 def finishEvent():
+    f = open('Day Tracker.txt', 'a+')   #Creates the Day Tracker.txt if it doesn't exist, or adds on to it
     nowFinish = time.asctime(time.localtime(time.time()))
     nowFinishTime = time.time()
     timeSpentMins = round(((nowFinishTime - startTimeSeconds[-1])/60), 0)
@@ -25,7 +25,7 @@ def finishEvent():
         f.write('From ' + startTime[-1] + ' to ' + nowFinish + ' I did: ' + eventList[-1] + ' for ' + str(timeSpentMins) + ' minutes' + '\n')
     else:
         f.write('From ' + startTime[-1] + ' to ' + nowFinish + ' I did: ' + eventList[-1] + ' for ' + str(timeSpentSeconds) + ' seconds' + '\n')
-
+    f.close()         #Done using the .txt file so I close it. This way the file is opened only when it is needed and can be accessed at other times
 #Runs all the functions in the necessary order and stuff
 def startProg():
     print 'Welcome to Day Tracker!'
@@ -33,13 +33,11 @@ def startProg():
     whileVar = ''
     while whileVar != 'q':
         addEvent()
-        whileVar = raw_input('Press y when finished with event or press q to quit...')
+        whileVar = raw_input('Press enter when finished with event or press q to quit...')
         finishEvent()
-    f.close()
     print 'Thank you for using Day Tracker! Have a nice day!~'
 
 #Starting program
 startProg()
 
 #TO-DO: #Add a way to see how much time has been spent on current event
-        #Add a way to access the .txt while program is running
